61c8c7694bdf4ebed2faef78f08ad218569bcd17
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-18 09:11:23 -0300

    Added descriptions to some keymappings

--- config/lua/core/remap.lua
+++ config/lua/core/remap.lua
@@ -10,9 +10,9 @@ vim.keymap.set("", "<Right>", "<nop>", options)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines without moving the cursor" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scrolls half a page down keeping the cursor in the middle" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scrolls half a page up keeping the cursor in the middle" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- config/lua/plugins/nvim-dap.lua
+++ config/lua/plugins/nvim-dap.lua
@@ -21,12 +21,14 @@ return {
             dapui.close()
         end
 
-        vim.keymap.set("n", "<leader>b", ":lua require('dap').toggle_breakpoint()<CR>")
-        vim.keymap.set("n", "<leader>B", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-        vim.keymap.set("n", "<F9>", ":lua require('dap').continue()<CR>")
-        vim.keymap.set("n", "<F8>", ":lua require('dap').step_over()<CR>")
-        vim.keymap.set("n", "<F7>", ":lua require('dap').step_into()<CR>")
-        vim.keymap.set("n", "<F6>", ":lua require('dap').step_out()<CR>")
-        vim.keymap.set("n", "<F5>", ":lua require('dap').repl_open()<CR>")
+        vim.keymap.set("n", "<leader>b", ":lua require('dap').toggle_breakpoint()<CR>",
+            { silent = true, desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<leader>B", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+            { silent = true, desc = "Set conditional breakpoint" })
+        vim.keymap.set("n", "<F9>", ":lua require('dap').continue()<CR>", { silent = true, desc = "Debug continue" })
+        vim.keymap.set("n", "<F8>", ":lua require('dap').step_over()<CR>", { silent = true, desc = "Debug step over" })
+        vim.keymap.set("n", "<F7>", ":lua require('dap').step_into()<CR>", { silent = true, desc = "Debug step into" })
+        vim.keymap.set("n", "<F6>", ":lua require('dap').step_out()<CR>", { silent = true, desc = "Debug step out" })
+        vim.keymap.set("n", "<F5>", ":lua require('dap').repl_open()<CR>", { silent = true, desc = "Debug open REPL" })
     end,
 }

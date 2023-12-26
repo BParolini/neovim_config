c87fd6619ced4faa799275ea5c5d046076d8aadf
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-26 16:47:29 -0300

    Multiple mappings and configurations

--- config/lua/plugins/telescope.lua
+++ config/lua/plugins/telescope.lua
@@ -10,10 +10,10 @@ return {
         telescope.load_extension("dap")
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>jj', function()
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find files", silent = true })
+        vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Telescope find git files", silent = true })
+        vim.keymap.set('n', '<leader>fj', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        end, { desc = "Telescope grep string", silent = true })
     end,
 }
--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     build = ":TSUpdate",
     config = function()
         require('nvim-treesitter.configs').setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
             ensure_installed = {
                 "c",
                 "cmake",

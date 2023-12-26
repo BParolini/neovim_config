aa2af0e8856964f1aa359c0f3917ce3ab111a39f
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-26 12:56:50 -0300

    Improved lsp_zero config

--- config/lua/plugins/nvim-lsp-zero.lua
+++ config/lua/plugins/nvim-lsp-zero.lua
@@ -16,6 +16,11 @@ return {
         -- Autocompletion
         "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lua",
+
+        -- Snippets
         "L3MON4D3/LuaSnip",
     },
     config = function()
@@ -50,7 +55,7 @@ return {
 
         local cmp = require('cmp')
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_mappings = lsp.defaults.cmp_mappings({
+        lsp.defaults.cmp_mappings({
             ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
             ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
             ['<C-y>'] = cmp.mapping.confirm({ select = true }),
@@ -62,7 +67,7 @@ return {
             local opts = { buffer = bufnr, remap = false }
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > ")) end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
             vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)

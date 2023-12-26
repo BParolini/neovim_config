855fbe60d118a6692d523eb34b362ed77e850c0b
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-26 14:47:14 -0300

    Fixed lsp mappings

--- config/lua/plugins/nvim-lsp-zero.lua
+++ config/lua/plugins/nvim-lsp-zero.lua
@@ -63,9 +63,10 @@ return {
         })
 
         lsp.on_attach(function(_, bufnr)
-            lsp.default_keymaps({ buffer = bufnr })
             local opts = { buffer = bufnr, remap = false }
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+            vim.keymap.set("n", "ge", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > ")) end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)

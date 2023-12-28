4e9cfeaeaf233b77d295894fe1c935e3973f74b0
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-28 12:08:15 -0300

    Adjusted missing keymap from jdtls

--- config/lua/plugins/nvim-lsp-zero.lua
+++ config/lua/plugins/nvim-lsp-zero.lua
@@ -108,33 +108,33 @@ return {
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local on_attach = function(_, bufnr)
             vim.keymap.set("n", "gd", vim.lsp.buf.definition,
-                { buffer = bufnr, remap = false, desc = "Go to definition", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to definition", silent = true })
             vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
-                { buffer = bufnr, remap = false, desc = "Go to implementation", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to implementation", silent = true })
             vim.keymap.set("n", "gD", vim.lsp.buf.declaration,
-                { buffer = bufnr, remap = false, desc = "Go to declaration", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to declaration", silent = true })
             vim.keymap.set("n", "gt", vim.lsp.buf.type_definition,
-                { buffer = bufnr, remap = false, desc = "Go to type declaration", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to type declaration", silent = true })
             vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references,
-                { buffer = bufnr, remap = false, desc = "Show references", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
             vim.keymap.set("n", "K", vim.lsp.buf.hover,
-                { buffer = bufnr, remap = false, desc = "Show documentation", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show documentation", silent = true })
             vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > ")) end,
-                { buffer = bufnr, remap = false, desc = "Find symbol", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Find symbol", silent = true })
             vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float,
-                { buffer = bufnr, remap = false, desc = "Show diagnostics window", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show diagnostics window", silent = true })
             vim.keymap.set("n", "[d", vim.diagnostic.goto_next,
-                { buffer = bufnr, remap = false, desc = "Go to next diagnostic", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to next diagnostic", silent = true })
             vim.keymap.set("n", "]d", vim.diagnostic.goto_prev,
-                { buffer = bufnr, remap = false, desc = "Go to previous diagnostic", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Go to previous diagnostic", silent = true })
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
-                { buffer = bufnr, remap = false, desc = "Show code actions", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show code actions", silent = true })
             vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references,
-                { buffer = bufnr, remap = false, desc = "Show references", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
             vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,
-                { buffer = bufnr, remap = false, desc = "Rename element", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Rename element", silent = true })
             vim.keymap.set("i", "<leader>he", vim.lsp.buf.signature_help,
-                { buffer = bufnr, remap = false, desc = "Show signature help", silent = true })
+                { buffer = bufnr, noremap = true, desc = "Show signature help", silent = true })
         end
 
         lspconfig.lua_ls.setup({
@@ -183,7 +183,7 @@ return {
 
         local lsps = {
             "bashls", "clangd", "cmake", "dockerls", "docker_compose_language_service",
-            "emmet_ls", "jsonls", "marksman", "ocamllsp", "rust_analyzer", "yamlls",
+            "emmet_ls", "jdtls", "jsonls", "marksman", "ocamllsp", "rust_analyzer", "yamlls",
         }
 
         for _, lsp_item in ipairs(lsps) do

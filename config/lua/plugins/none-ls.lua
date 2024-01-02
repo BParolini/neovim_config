f7d223997d8c5af8c2a354c2a40502f685089ed2
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-02 16:34:15 -0300

    Added markdown formatter/lint

--- config/lua/plugins/none-ls.lua
+++ config/lua/plugins/none-ls.lua
@@ -18,12 +18,14 @@ return {
                 null_ls.builtins.formatting.ocamlformat,
                 null_ls.builtins.formatting.rustfmt,
                 null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.markdownlint,
 
                 null_ls.builtins.diagnostics.luacheck,
                 null_ls.builtins.diagnostics.flake8,
                 null_ls.builtins.diagnostics.shellcheck,
                 null_ls.builtins.diagnostics.yamllint,
                 null_ls.builtins.diagnostics.cmake_lint,
+                null_ls.builtins.diagnostics.markdownlint,
             },
         })
     end,

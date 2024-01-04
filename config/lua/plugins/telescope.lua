b80e4f5082543938752649137c8e281658ae7f8a
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-04 20:51:22 -0300

    Added two new keymappings to telescope

--- config/lua/plugins/telescope.lua
+++ config/lua/plugins/telescope.lua
@@ -11,13 +11,21 @@ return {
             telescope.load_extension("harpoon")
             telescope.load_extension("dap")
 
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find files", silent = true })
-            vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Telescope find git files", silent = true })
-            vim.keymap.set('n', '<leader>fj', function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files", silent = true })
+            vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope find git files", silent = true })
+            vim.keymap.set("n", "<leader>fj", function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
             end, { desc = "Telescope grep string", silent = true })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help tags", silent = true })
+            vim.keymap.set("n", "<leader>fw", function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Telescope grep word under cursor (strictly delimited)", silent = true })
+            vim.keymap.set("n", "<leader>fW", function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Telescope grep word under cursor (blank space delimited)", silent = true })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags", silent = true })
         end,
     },
     {
@@ -29,7 +37,7 @@ return {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
                     },
-                }
+                },
             })
             telescope.load_extension("ui-select")
         end,

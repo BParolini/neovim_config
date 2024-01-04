857f848c4996577a29a3818559d95737dea88d47
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-04 12:09:46 -0300

    Included auto formatting to none-ls

--- config/lua/plugins/none-ls.lua
+++ config/lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
         null_ls.setup({
             sources = {
@@ -18,6 +19,8 @@ return {
                 null_ls.builtins.formatting.ocamlformat,
                 null_ls.builtins.formatting.rustfmt,
                 null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.golines,
                 null_ls.builtins.formatting.markdownlint,
 
                 null_ls.builtins.diagnostics.luacheck,
@@ -27,6 +30,21 @@ return {
                 null_ls.builtins.diagnostics.cmake_lint,
                 null_ls.builtins.diagnostics.markdownlint,
             },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr,
+                    })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
         })
     end,
 }

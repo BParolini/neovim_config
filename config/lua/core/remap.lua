0e0084cfa29a7cfe1fedc8aa7e524db975ca28c6
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-18 22:42:17 -0300

    Removed unused keymappings

--- config/lua/core/remap.lua
+++ config/lua/core/remap.lua
@@ -16,15 +16,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scrolls half a page up keeping
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>gg", vim.lsp.buf.format, { desc = "Format current buffer" })
 

b5e5177b3e636094e8080a45469cc4eb2b835da2
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-04 08:36:43 -0300

    Documented extra key chains

--- config/lua/plugins/which-key.lua
+++ config/lua/plugins/which-key.lua
@@ -6,5 +6,15 @@ return {
         o.timeout = true
         o.timeoutlen = 300
     end,
-    opts = {},
+    config = function()
+        require("which-key").setup()
+
+        require("which-key").register({
+            ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+            ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+            ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+            ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+            ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+        })
+    end,
 }

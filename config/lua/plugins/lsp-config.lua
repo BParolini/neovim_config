9bc1ea959386355c5de6f5c07a4403dc6312184f
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-04 08:34:49 -0300

    Reconfigured capabilities to better utilize all options available

--- config/lua/plugins/lsp-config.lua
+++ config/lua/plugins/lsp-config.lua
@@ -44,6 +44,7 @@ return {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "folke/neodev.nvim",
+            { "j-hui/fidget.nvim", opts = {} },
         },
         config = function()
             require("neodev").setup({
@@ -59,39 +60,28 @@ return {
             local lspconfig = require("lspconfig")
             local util = require("lspconfig/util")
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
             local on_attach = function(_, bufnr)
                 local keymap, lsp, fn, diagnostic = vim.keymap, vim.lsp, vim.fn, vim.diagnostic
 
-                keymap.set("n", "gd", lsp.buf.definition,
-                    { buffer = bufnr, noremap = true, desc = "Go to definition", silent = true })
-                keymap.set("n", "gi", lsp.buf.implementation,
-                    { buffer = bufnr, noremap = true, desc = "Go to implementation", silent = true })
-                keymap.set("n", "gD", lsp.buf.declaration,
-                    { buffer = bufnr, noremap = true, desc = "Go to declaration", silent = true })
-                keymap.set("n", "gt", lsp.buf.type_definition,
-                    { buffer = bufnr, noremap = true, desc = "Go to type declaration", silent = true })
-                keymap.set("n", "gr", require("telescope.builtin").lsp_references,
-                    { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
-                keymap.set("n", "K", lsp.buf.hover,
-                    { buffer = bufnr, noremap = true, desc = "Show documentation", silent = true })
+                keymap.set("n", "gd", lsp.buf.definition, { buffer = bufnr, noremap = true, desc = "Go to definition", silent = true })
+                keymap.set("n", "gi", lsp.buf.implementation, { buffer = bufnr, noremap = true, desc = "Go to implementation", silent = true })
+                keymap.set("n", "gD", lsp.buf.declaration, { buffer = bufnr, noremap = true, desc = "Go to declaration", silent = true })
+                keymap.set("n", "gt", lsp.buf.type_definition, { buffer = bufnr, noremap = true, desc = "Go to type declaration", silent = true })
+                keymap.set("n", "gr", require("telescope.builtin").lsp_references, { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
+                keymap.set("n", "K", lsp.buf.hover, { buffer = bufnr, noremap = true, desc = "Show documentation", silent = true })
                 keymap.set("n", "<leader>ws", function()
                     lsp.buf.workspace_symbol(fn.input("Grep > "))
                 end, { buffer = bufnr, noremap = true, desc = "Find symbol", silent = true })
-                keymap.set("n", "<leader>vd", diagnostic.open_float,
-                    { buffer = bufnr, noremap = true, desc = "Show diagnostics window", silent = true })
-                keymap.set("n", "[d", diagnostic.goto_prev,
-                    { buffer = bufnr, noremap = true, desc = "Go to previous diagnostic", silent = true })
-                keymap.set("n", "]d", diagnostic.goto_next,
-                    { buffer = bufnr, noremap = true, desc = "Go to next diagnostic", silent = true })
-                keymap.set("n", "<leader>ca", lsp.buf.code_action,
-                    { buffer = bufnr, noremap = true, desc = "Show code actions", silent = true })
-                keymap.set("n", "<leader>vrr", lsp.buf.references,
-                    { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
-                keymap.set("n", "<leader>rn", lsp.buf.rename,
-                    { buffer = bufnr, noremap = true, desc = "Rename element", silent = true })
-                keymap.set("i", "<leader>he", lsp.buf.signature_help,
-                    { buffer = bufnr, noremap = true, desc = "Show signature help", silent = true })
+                keymap.set("n", "<leader>vd", diagnostic.open_float, { buffer = bufnr, noremap = true, desc = "Show diagnostics window", silent = true })
+                keymap.set("n", "[d", diagnostic.goto_prev, { buffer = bufnr, noremap = true, desc = "Go to previous diagnostic", silent = true })
+                keymap.set("n", "]d", diagnostic.goto_next, { buffer = bufnr, noremap = true, desc = "Go to next diagnostic", silent = true })
+                keymap.set("n", "<leader>ca", lsp.buf.code_action, { buffer = bufnr, noremap = true, desc = "Show code actions", silent = true })
+                keymap.set("n", "<leader>vrr", lsp.buf.references, { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
+                keymap.set("n", "<leader>rn", lsp.buf.rename, { buffer = bufnr, noremap = true, desc = "Rename element", silent = true })
+                keymap.set("i", "<leader>he", lsp.buf.signature_help, { buffer = bufnr, noremap = true, desc = "Show signature help", silent = true })
             end
 
             lspconfig.gopls.setup({

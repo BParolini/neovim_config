dc547c7b34f1932bfd33cb9d8336dcbfdbdd3f73
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-04 08:34:03 -0300

    Changed signs in gitsigns

--- config/lua/plugins/gitsigns.lua
+++ config/lua/plugins/gitsigns.lua
@@ -1,11 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
-    config = function()
+    config = function(_, opts)
         local gitsigns = require("gitsigns")
-        gitsigns.setup()
+        gitsigns.setup(opts)
 
         local keymap = vim.keymap
         keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { noremap = true, desc = "Preview git changes" })
         keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { noremap = true, desc = "Show git blame for the current line" })
     end,
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "_" },
+            topdelete = { text = "‾" },
+            changedelete = { text = "≃" },
+        },
+    },
 }

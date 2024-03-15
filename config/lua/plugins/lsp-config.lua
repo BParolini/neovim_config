c9077a9d1acbcd3e8b8c2fb00ed2493a169bd4bb
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-15 14:03:01 -0300

    Configured `html`, `htmx` and `templ` LSPs

--- config/lua/plugins/lsp-config.lua
+++ config/lua/plugins/lsp-config.lua
@@ -25,6 +25,7 @@ return {
                     "emmet_ls",
                     "gopls",
                     "hls",
+                    "html",
                     "htmx",
                     "jdtls",
                     "jsonls",
@@ -105,25 +106,41 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+            --- Prepares `on_attach` for LSP clientside
+            --- @param client lsp.Client
+            --- @param bufnr number
             local on_attach = function(client, bufnr)
                 local keymap, lsp, fn, diagnostic = vim.keymap, vim.lsp, vim.fn, vim.diagnostic
 
-                keymap.set("n", "gd", lsp.buf.definition, { buffer = bufnr, noremap = true, desc = "Go to definition", silent = true })
-                keymap.set("n", "gi", lsp.buf.implementation, { buffer = bufnr, noremap = true, desc = "Go to implementation", silent = true })
-                keymap.set("n", "gD", lsp.buf.declaration, { buffer = bufnr, noremap = true, desc = "Go to declaration", silent = true })
-                keymap.set("n", "gt", lsp.buf.type_definition, { buffer = bufnr, noremap = true, desc = "Go to type declaration", silent = true })
-                keymap.set("n", "gr", require("telescope.builtin").lsp_references, { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
-                keymap.set("n", "K", lsp.buf.hover, { buffer = bufnr, noremap = true, desc = "Show documentation", silent = true })
+                keymap.set("n", "gd", lsp.buf.definition,
+                    { buffer = bufnr, noremap = true, desc = "Go to definition", silent = true })
+                keymap.set("n", "gi", lsp.buf.implementation,
+                    { buffer = bufnr, noremap = true, desc = "Go to implementation", silent = true })
+                keymap.set("n", "gD", lsp.buf.declaration,
+                    { buffer = bufnr, noremap = true, desc = "Go to declaration", silent = true })
+                keymap.set("n", "gt", lsp.buf.type_definition,
+                    { buffer = bufnr, noremap = true, desc = "Go to type declaration", silent = true })
+                keymap.set("n", "gr", require("telescope.builtin").lsp_references,
+                    { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
+                keymap.set("n", "K", lsp.buf.hover,
+                    { buffer = bufnr, noremap = true, desc = "Show documentation", silent = true })
                 keymap.set("n", "<leader>ws", function()
                     lsp.buf.workspace_symbol(fn.input("Grep > "))
                 end, { buffer = bufnr, noremap = true, desc = "Find symbol", silent = true })
-                keymap.set("n", "<leader>vd", diagnostic.open_float, { buffer = bufnr, noremap = true, desc = "Show diagnostics window", silent = true })
-                keymap.set("n", "[d", diagnostic.goto_prev, { buffer = bufnr, noremap = true, desc = "Go to previous diagnostic", silent = true })
-                keymap.set("n", "]d", diagnostic.goto_next, { buffer = bufnr, noremap = true, desc = "Go to next diagnostic", silent = true })
-                keymap.set("n", "<leader>ca", lsp.buf.code_action, { buffer = bufnr, noremap = true, desc = "Show code actions", silent = true })
-                keymap.set("n", "<leader>vrr", lsp.buf.references, { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
-                keymap.set("n", "<leader>rn", lsp.buf.rename, { buffer = bufnr, noremap = true, desc = "Rename element", silent = true })
-                keymap.set("i", "<leader>he", lsp.buf.signature_help, { buffer = bufnr, noremap = true, desc = "Show signature help", silent = true })
+                keymap.set("n", "<leader>vd", diagnostic.open_float,
+                    { buffer = bufnr, noremap = true, desc = "Show diagnostics window", silent = true })
+                keymap.set("n", "[d", diagnostic.goto_prev,
+                    { buffer = bufnr, noremap = true, desc = "Go to previous diagnostic", silent = true })
+                keymap.set("n", "]d", diagnostic.goto_next,
+                    { buffer = bufnr, noremap = true, desc = "Go to next diagnostic", silent = true })
+                keymap.set("n", "<leader>ca", lsp.buf.code_action,
+                    { buffer = bufnr, noremap = true, desc = "Show code actions", silent = true })
+                keymap.set("n", "<leader>vrr", lsp.buf.references,
+                    { buffer = bufnr, noremap = true, desc = "Show references", silent = true })
+                keymap.set("n", "<leader>rn", lsp.buf.rename,
+                    { buffer = bufnr, noremap = true, desc = "Rename element", silent = true })
+                keymap.set("n", "<leader>he", lsp.buf.signature_help,
+                    { buffer = bufnr, noremap = true, desc = "Show signature help", silent = true })
 
                 require("autocmd.formatting").create_formatting_augroup(client, bufnr)
             end
@@ -179,6 +196,15 @@ return {
                 },
             })
 
+            local html_lsps = { "html", "htmx" }
+            for _, lsp in ipairs(html_lsps) do
+                lspconfig[lsp].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    filetypes = { lsp, "templ" },
+                })
+            end
+
             local lsps = {
                 "bashls",
                 "clangd",
@@ -187,7 +213,6 @@ return {
                 "dockerls",
                 "emmet_ls",
                 "hls",
-                "htmx",
                 "jdtls",
                 "jsonls",
                 "kotlin_language_server",
@@ -204,6 +229,8 @@ return {
                     capabilities = capabilities,
                 })
             end
+
+            vim.filetype.add({ extension = { templ = "templ" } })
         end,
     },
     {

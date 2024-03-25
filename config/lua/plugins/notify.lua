2902ee5c6e774cb52cbe82cb214b7b7335f8dc6a
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-25 10:13:58 -0300

    Configured notify plugin

--- config/lua/plugins/notify.lua
+++ config/lua/plugins/notify.lua
@@ -1,6 +1,19 @@
 return {
     "rcarriga/nvim-notify",
     config = function()
-        vim.notify = require("notify")
+        local notify = require("notify")
+        notify.setup({
+            stages = "fade",
+            timeout = 5000,
+            background_colour = "#000000",
+            icons = {
+                ERROR = "",
+                WARN = "",
+                INFO = "",
+                DEBUG = "",
+                TRACE = "✎",
+            },
+        })
+        vim.notify = notify
     end,
 }

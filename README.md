e8f9593e28f6a52249ef94b707a86b785720c03d
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-26 22:05:51 -0300

    Added multiple plugins

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ All plugins are managed with [Lazy](https://github.com/folke/lazy.nvim).
 
 ### Installed plugins
 
+- [Auto Pairs](https://github.com/windwp/nvim-autopairs)
 - [Catppuccin](https://github.com/catppuccin/nvim)
 - [Cloak](https://github.com/laytan/cloak.nvim)
+- [Comment](https://github.com/numToStr/Comment.nvim)
 - Completions
   - [cmp-nvim-lsp](https://github.com/hrsh7th/cmp-nvim-lsp)
   - [LuaSnip](https://github.com/L3MON4D3/LuaSnip)
@@ -21,9 +23,12 @@ All plugins are managed with [Lazy](https://github.com/folke/lazy.nvim).
     - [cmp-nvim-lua](https://github.com/hrsh7th/cmp-nvim-lua)
   - [crates.nvim](https://github.com/Saecki/crates.nvim)
 - [Dev Icons](https://github.com/ryanoasis/vim-devicons)
+- [Flash](https://github.com/folke/flash.nvim)
 - [GH Copilot](https://github.com/github/copilot.vim)
 - [Git Signs](https://github.com/lewis6991/gitsigns.nvim)
 - [Harpoon](https://github.com/ThePrimeagen/harpoon)
+- [Highlights](https://github.com/folke/todo-comments.nvim)
+- [Indent](https://github.com/echasnovski/mini.nvim)
 - LSP Config
   - [mason](https://github.com/williamboman/mason.nvim)
   - [mason-lspconfig](https://github.com/williamboman/mason-lspconfig.nvim)
@@ -45,6 +50,7 @@ All plugins are managed with [Lazy](https://github.com/folke/lazy.nvim).
   - [nvim-dap-python](https://github.com/mfussenegger/nvim-dap-python)
   - [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
   - [telescope-dap.nvim](https://github.com/nvim-telescope/telescope-dap.nvim)
+- [Surround](https://github.com/kylechui/nvim-surround)
 - [Telescope](https://github.com/nvim-telescope/telescope.nvim)
   - [plenary.nvim](https://github.com/nvim-lua/plenary.nvim)
 - [Treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
--- config/lua/plugins/treesitter.lua
+++ config/lua/plugins/treesitter.lua
@@ -1,72 +1,80 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "c",
-                "cmake",
-                "cpp",
-                "diff",
-                "dockerfile",
-                "gitignore",
-                "go",
-                "gomod",
-                "gosum",
-                "gotmpl",
-                "groovy",
-                "html",
-                "java",
-                "javascript",
-                "json",
-                "kotlin",
-                "lua",
-                "luadoc",
-                "make",
-                "markdown",
-                "ocaml",
-                "properties",
-                "proto",
-                "python",
-                "rust",
-                "sql",
-                "templ",
-                "tmux",
-                "toml",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true,
-            },
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "cmake",
+                    "cpp",
+                    "diff",
+                    "dockerfile",
+                    "gitignore",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "gotmpl",
+                    "groovy",
+                    "html",
+                    "java",
+                    "javascript",
+                    "json",
+                    "kotlin",
+                    "lua",
+                    "luadoc",
+                    "make",
+                    "markdown",
+                    "ocaml",
+                    "properties",
+                    "proto",
+                    "python",
+                    "rust",
+                    "sql",
+                    "templ",
+                    "tmux",
+                    "toml",
+                    "typescript",
+                    "vim",
+                    "yaml",
                 },
-            },
-        })
-    end,
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                indent = {
+                    enable = true,
+                },
+
+                highlight = {
+                    enable = true,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
 }

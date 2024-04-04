32143eaacf17459af132b567e0b02dc2ef9c19e2
Bruno Parolini <bruno.parolini@olxbr.com>
2024-04-03 22:52:38 -0300

    Refactored neotree keymaps

--- config/lua/plugins/neo-tree.lua
+++ config/lua/plugins/neo-tree.lua
@@ -7,7 +7,14 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set('n', '<C-m>', ':Neotree toggle<CR>',
-            { silent = true, noremap = true, desc = "Opens Neotree" })
+        require("neo-tree").setup({
+            filesystem = {
+                use_libuv_file_watcher = true,
+            },
+        })
+        local keymap = vim.keymap
+        keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<CR>", { silent = true, noremap = true, desc = "Toggle file explorer" })
+        keymap.set("n", "<leader>ef", "<cmd>Neotree reveal<CR>", { silent = true, noremap = true, desc = "Toggle file explorer on current file" })
+        keymap.set("n", "<leader>ec", "<cmd>Neotree close<CR>", { silent = true, noremap = true, desc = "Collapse file explorer" })
     end,
 }

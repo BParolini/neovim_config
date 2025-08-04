20b3cdb601ba3e04b629ac743380b6b8156746a0
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-03 21:43:33 -0300

    Adjusted noice config

--- nvim/dot-config/nvim/lua/plugins/noice.lua
+++ nvim/dot-config/nvim/lua/plugins/noice.lua
@@ -1,15 +1,23 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp,
+            },
+        },
+    },
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
+        "nvim-treesitter/nvim-treesitter",
     },
     config = function(_, opts)
         require("noice").setup(opts)
 
-        vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>",
-            { noremap = true, silent = true, desc = "Dismiss Noice Messages" })
+        vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { noremap = true, silent = true, desc = "Dismiss Noice Messages" })
     end,
 }

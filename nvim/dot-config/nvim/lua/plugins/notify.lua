cea56ea8e4b3b7a54ed4ea58aeea96cb299e3318
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-03 21:43:26 -0300

    Adjusted notify config

--- nvim/dot-config/nvim/lua/plugins/notify.lua
+++ nvim/dot-config/nvim/lua/plugins/notify.lua
@@ -1,19 +1,11 @@
 return {
     "rcarriga/nvim-notify",
-    config = function()
+    opts = {
+        background_colour = "#000000",
+    },
+    config = function(_, opts)
         local notify = require("notify")
-        notify.setup({
-            stages = "fade",
-            timeout = 5000,
-            background_colour = "#000000",
-            icons = {
-                ERROR = "",
-                WARN = "",
-                INFO = "",
-                DEBUG = "",
-                TRACE = "✎",
-            },
-        })
+        notify.setup(opts)
         vim.notify = notify
     end,
 }

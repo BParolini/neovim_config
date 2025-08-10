010f7ba4a3ce3288d2b15318bc43c0ee83201394
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-10 18:07:23 -0300

    Updated which-key appearance

--- nvim/dot-config/nvim/lua/plugins/which-key.lua
+++ nvim/dot-config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,15 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     init = function()
         local o = vim.o
         o.timeout = true
         o.timeoutlen = 300
     end,
-    opts = {},
+    opts = {
+        preset = "modern",
+    },
 }

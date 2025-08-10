e9f83216d620b4609a703ee5344f5b37c1136691
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-10 18:08:20 -0300

    Code formatting adjustments

--- nvim/dot-config/nvim/lua/plugins/lazydev.lua
+++ nvim/dot-config/nvim/lua/plugins/lazydev.lua
@@ -1,17 +1,16 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        dependencies = {
-            "justinsgithub/wezterm-types",
+    "folke/lazydev.nvim",
+    dependencies = {
+        "justinsgithub/wezterm-types",
+    },
+    ft = "lua", -- only load on lua files
+    opts = {
+        library = {
+            "lazy.nvim",
+            "LazyVim",
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            { path = "wezterm-types", mods = { "wezterm" } },
         },
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                "lazy.nvim",
-                "LazyVim",
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                { path = "wezterm-types",      mods = { "wezterm" } },
-            },
-        },
+        enabled = true,
     },
 }

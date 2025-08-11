21da5cb92ca360db90ae10b927de1175578f9c64
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-10 21:09:49 -0300

    Adjustments to conform
    Changed python formatters to black

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,7 @@
 return {
     "stevearc/conform.nvim",
     lazy = false,
+    --- @type conform.setupOpts
     opts = {
         formatters_by_ft = {
             bash = { "beautysh" },
@@ -8,30 +9,35 @@ return {
             closjure = { "cljfmt" },
             cmake = { "cmake_format" },
             cpp = { "clang-format" },
+            css = { "prettierd", "prettier", stop_after_first = true },
             go = { "goimports", "golines", "gofmt" },
+            html = { "prettierd", "prettier", stop_after_first = true },
             java = { "google-java-format" },
             javascript = { "prettierd", "prettier", stop_after_first = true },
             json = { "jq" },
+            less = { "prettierd", "prettier", stop_after_first = true },
             lua = { "stylua" },
             markdown = { "markdownlint" },
             ocaml = { "ocamlformat" },
             proto = { "buf" },
-            python = { "isort", "autopep8" },
+            python = { "black" },
             rust = { "rustfmt", lsp_format = "fallback" },
+            scss = { "prettierd", "prettier", stop_after_first = true },
             templ = { "templ" },
             terraform = { "terraform_fmt" },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
             xml = { "xmlformatter" },
             yaml = { "yamlfmt" },
-            css = { "prettierd", "prettier", stop_after_first = true },
-            html = { "prettierd", "prettier", stop_after_first = true },
-            scss = { "prettierd", "prettier", stop_after_first = true },
-            less = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        formatters = {
+            black = {
+                prepend_args = { "--fast" },
+            },
         },
     },
     format_on_save = {
         -- These options will be passed to conform.format()
-        timeout_ms = 500,
+        timeout_ms = 2000,
         lsp_format = "fallback",
     },
     ---@type LazyKeysSpec[]

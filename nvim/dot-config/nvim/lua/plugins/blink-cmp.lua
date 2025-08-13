e870a52b7c254e6ba45346115776dd2967efb1b1
Bruno Parolini <bruno.parolini@olxbr.com>
2025-08-13 16:43:15 -0300

    Added emoji completions for markdown and git commit messages

--- nvim/dot-config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/dot-config/nvim/lua/plugins/blink-cmp.lua
@@ -2,7 +2,10 @@
 return {
     "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        "moyiz/blink-emoji.nvim",
+    },
     -- use a release tag to download pre-built binaries
     version = "1.*",
     --- @module 'blink.cmp'
@@ -28,12 +31,32 @@ return {
             enabled = true,
         },
         sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            default = { "lazydev", "lsp", "path", "snippets", "buffer", "emoji" },
             providers = {
                 lazydev = {
                     name = "LazyDev",
                     module = "lazydev.integrations.blink",
                     score_offset = 100,
+                },
+                emoji = {
+                    module = "blink-emoji",
+                    name = "Emoji",
+                    score_offset = 15,
+                    opts = {
+                        insert = true, -- Insert emoji (default) or complete its name
+                        ---@type string|table|fun():table
+                        trigger = function()
+                            return { ":" }
+                        end,
+                    },
+                    should_show_items = function()
+                        return vim.tbl_contains(
+                            -- Enable emoji completion only for git commits and markdown.
+                            -- By default, enabled for all file-types.
+                            { "gitcommit", "markdown" },
+                            vim.o.filetype
+                        )
+                    end,
                 },
             },
         },

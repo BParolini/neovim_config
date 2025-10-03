83974fba8e59361a7d479b99debf55ce9f5b71b9
Bruno Parolini <brunoparolini@gmail.com>
2025-10-03 09:36:36 -0300

    Commented out `nvim-dap-python` for it is not working in MELI

--- nvim/dot-config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-dap.lua
@@ -109,16 +109,16 @@ return {
             "rcarriga/nvim-dap-ui",
         },
     },
-    {
-        "mfussenegger/nvim-dap-python",
-        ft = "python",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
-        },
-        config = function(_, opts)
-            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-            require("dap-python").setup(path, opts)
-        end,
-    },
+    -- {
+    --     "mfussenegger/nvim-dap-python",
+    --     ft = "python",
+    --     dependencies = {
+    --         "mfussenegger/nvim-dap",
+    --         "rcarriga/nvim-dap-ui",
+    --     },
+    --     config = function(_, opts)
+    --         local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+    --         require("dap-python").setup(path, opts)
+    --     end,
+    -- },
 }
